d2d2f1ea855b654ed1ee7559c94d4a1c6696d262
Martin Egli <martin@eglinet.ch>
2020-02-08 22:10:57 +0100

    Update

---
+++ Source/Anori.WPF.Behaviors/EventTriggerCreator.cs
@@ -0,0 +1,16 @@
+﻿namespace Anori.WPF.Behaviors
+{
+    public class EventTriggerCreator : ITriggerCreator
+    {
+        public TriggerBase Create()
+        {
+            return new EventTrigger(EventName);
+        }
+
+        public string EventName
+        {
+            get;
+            set;
+        }
+    }
+}
