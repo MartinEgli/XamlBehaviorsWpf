fc2825bad91d05c665c5f9bf5be35f7775490886
Martin Egli <martin@eglinet.ch>
2020-02-17 20:58:31 +0100

    Update

--- Source/Anori.WPF.WeakEventManagers/LoadedWeakEventManager - Copy.cs
+++ Source/Anori.WPF.WeakEventManagers/LoadedWeakEventManager - Copy.cs
@@ -1,136 +1,87 @@
-﻿namespace Anori.WPF.WeakEventManagers
+﻿using System;
+using System.Windows;
+
+namespace Anori.WPF.WeakEventManagers
 {
     #region
 
-    using System;
-    using System.Windows;
-
-    using Anori.WPF.Interactivities.WeakEventManagers;
-
-    using JetBrains.Annotations;
-
     #endregion
+
+    public class DataContextChangedObservable
+    {
+        private FrameworkElement frameworkElement;
+
+        public DataContextChangedObservable(FrameworkElement frameworkElement)
+        {
+            this.frameworkElement = frameworkElement;
+        }
+
+        public event EventHandler<DataContextChangedObservableArgs> DataContextChanged;
+    }
+
+    public class DataContextChangedObservableArgs : EventArgs
+    {
+        public DataContextChangedObservableArgs(DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            this.DependencyPropertyChangedEventArgs = dependencyPropertyChangedEventArgs;
+        }
+
+        public DependencyPropertyChangedEventArgs DependencyPropertyChangedEventArgs { get; }
+    }
 
     /// <inheritdoc />
     /// <summary>
-    /// Loaded WeakEventManager Class
+    ///     Loaded WeakEventManager Class
     /// </summary>
     /// <seealso cref="T:System.Windows.WeakEventManager" />
-    public sealed class DataContextChangedWeakEventManager : WeakEventManager<FrameworkElement,>
+    public static class DataContextChangedEventManager
     {
-        /// <inheritdoc />
-        /// <summary>
-        /// Prevents a default instance of the <see cref="T:Anori.WPF.Interactivities.WeakEventManagers.LoadedWeakEventManager" /> class from being created.
-        /// </summary>
-        private DataContextChangedWeakEventManager()
+        public static IDisposable Subscribe(FrameworkElement frameworkElement,
+            EventHandler<DependencyPropertyChangedEventArgs> handler)
         {
+            EventHandler<DataContextChangedObservableArgs> Handler = (sender, args) =>
+                handler.Invoke(sender, args.DependencyPropertyChangedEventArgs);
+
+            DataContextChangedObservable observable = new DataContextChangedObservable(frameworkElement);
+            WeakReference<DataContextChangedObservable> weakObservable =
+                new WeakReference<DataContextChangedObservable>(observable);
+            WeakReference<EventHandler<DataContextChangedObservableArgs>> weakHandler =
+                new WeakReference<EventHandler<DataContextChangedObservableArgs>>(Handler);
+
+            WeakEventManager<DataContextChangedObservable, DataContextChangedObservableArgs>.AddHandler(observable,
+                nameof(DataContextChangedObservable.DataContextChanged), Handler);
+
+            return new Disposable(() =>
+            {
+                if (!weakObservable.TryGetTarget(out DataContextChangedObservable o))
+                {
+                    return;
+                }
+
+                if (!weakHandler.TryGetTarget(out EventHandler<DataContextChangedObservableArgs> h))
+                {
+                    return;
+                }
+
+                WeakEventManager<DataContextChangedObservable, DataContextChangedObservableArgs>.RemoveHandler(
+                    o,
+                    nameof(DataContextChangedObservable.DataContextChanged), h);
+            });
+        }
+    }
+
+    public class Disposable : IDisposable
+    {
+        private readonly Action action;
+
+        public Disposable(Action action)
+        {
+            this.action = action;
         }
 
-        /// <summary>
-        /// Gets the current manager.
-        /// </summary>
-        /// <returns></returns>
-        private static DataContextChangedWeakEventManager GetCurrentManager()
+        public void Dispose()
         {
-            var type = typeof(DataContextChangedWeakEventManager);
-
-            if (WeakEventManager.GetCurrentManager(type) is DataContextChangedWeakEventManager manager)
-            {
-                return manager;
-            }
-
-            manager = new DataContextChangedWeakEventManager();
-            SetCurrentManager(type, manager);
-
-            return manager;
-        }
-
-        /// <summary>
-        /// Adds the listener.
-        /// </summary>
-        /// <param name="source">The source.</param>
-        /// <param name="listener">The listener.</param>
-        /// <exception cref="ArgumentNullException">
-        /// source
-        /// or
-        /// listener
-        /// </exception>
-        public static void AddListener([NotNull] FrameworkElement source, [NotNull] IWeakEventListener listener)
-        {
-            if (source == null)
-            {
-                throw new ArgumentNullException(nameof(source));
-            }
-
-            if (listener == null)
-            {
-                throw new ArgumentNullException(nameof(listener));
-            }
-
-            GetCurrentManager().ProtectedAddListener(source, listener);
-        }
-
-        /// <summary>
-        /// Removes the listener.
-        /// </summary>
-        /// <param name="source">The source.</param>
-        /// <param name="listener">The listener.</param>
-        /// <exception cref="ArgumentNullException">
-        /// source
-        /// or
-        /// listener
-        /// </exception>
-        public static void RemoveListener([NotNull] FrameworkElement source, [NotNull] IWeakEventListener listener)
-        {
-            if (source == null)
-            {
-                throw new ArgumentNullException(nameof(source));
-            }
-
-            if (listener == null)
-            {
-                throw new ArgumentNullException(nameof(listener));
-            }
-
-            GetCurrentManager().ProtectedRemoveListener(source, listener);
-        }
-
-        /// <summary>
-        /// Called when [loaded].
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="dependencyPropertyChangedEventArgs">The <see cref="DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
-        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
-        {
-            this.DeliverEvent(sender, dependencyPropertyChangedEventArgs);
-        }
-
-        /// <inheritdoc />
-        /// <summary>
-        /// When overridden in a derived class, starts listening for the event being managed.
-        /// After <see cref="M:System.Windows.WeakEventManager.StartListening(System.Object)" /> is first called,
-        /// the manager should be in the state of calling <see cref="M:System.Windows.WeakEventManager.DeliverEvent(System.Object,System.EventArgs)" /> or <see cref="M:System.Windows.WeakEventManager.DeliverEventToList(System.Object,System.EventArgs,System.Windows.WeakEventManager.ListenerList)" /> whenever the relevant event from the provided source is handled.
-        /// </summary>
-        /// <param name="source">The source to begin listening on.</param>
-        protected override void StartListening(object source)
-        {
-            if (source is FrameworkElement trigger)
-            {
-                trigger.DataContextChanged += this.OnDataContextChanged;
-            }
-        }
-
-        /// <inheritdoc />
-        /// <summary>
-        /// When overridden in a derived class, stops listening on the provided source for the event being managed.
-        /// </summary>
-        /// <param name="source">The source to stop listening on.</param>
-        protected override void StopListening(object source)
-        {
-            if (source is FrameworkElement trigger)
-            {
-                trigger.DataContextChanged -= this.OnDataContextChanged;
-            }
+            this.action?.Invoke();
         }
     }
 }
